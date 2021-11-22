263f68f87a57619ed9403aa5963434b20e45c720
karl.feng <Fengka@ssfs.org>
2021-11-21 21:42:49 -0500

    2.5 practice

--- src/Names.java
+++ src/Names.java
@@ -3,17 +3,9 @@ import java.util.Scanner;
 public class Names {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+
 
         // Get the first and last names as separate variables (use the Scanner object to get the input)
-
-           String lastnamen = "Feng";
-           String firstname = "Kang";
-
-           System.out.println("Karl Feng");
-           System.out.println("FK");
-           System.out.println();
-
 
 
 
--- src/WrappersDelight.java
+++ src/WrappersDelight.java
@@ -1,12 +1,21 @@
-public class                                                                                                                                                                                                                                                            WrappersDelight {
+  public class Wrappers {                                                                                                                                                                                                                                      WrappersDelight {
 
     // Create a main class and follow the instructions on the Lab sheet to complete this assignment.
     // When finished, uncomment the WrappersDelight.main(null) line in the
     // test file to make sure your program is correct.
 
-    Integer num1 = 14;
+    int num1 = 14;
     Integer num2 = 8;
     double num3 = 2.5;
+    Double num4 = (double)num3+(double) num2;
+    num2 = 5;
+    num3= num1+num2+num4;
+    num1 = (int)(num2+ num4);
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(num3);
+    System.out.println(num4);
+    }
 
 
 }
